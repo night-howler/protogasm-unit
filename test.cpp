6ac2061d36de69a89400e532b993ac9fdd443465
Night Howler <34203458+night-howler@users.noreply.github.com>
2021-11-21 18:15:14 -0800

    import protogasm code

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <ArduinoFake.h>
 #include <unity.h>
 
+
 using namespace fakeit;
 
 void setUp(void)
@@ -11,9 +12,23 @@ void setUp(void)
 
 void test_loop(void)
 {
+}
+
+void test_setup(void)
+{
+    When(Method(ArduinoFake(), pinMode)).AlwaysReturn();
+    When(Method(ArduinoFake(), digitalWrite)).AlwaysReturn();
+    When(Method(ArduinoFake(), analogReference)).AlwaysReturn();
+    //When(Method(ArduinoFake(Serial), begin)).AlwaysReturn();
+    When(OverloadedMethod(ArduinoFake(Serial), begin, void(unsigned long))).AlwaysReturn();
+    //When(Method(ArduinoFake(raPressure), clear)).AlwaysReturn();
     When(Method(ArduinoFake(), delay)).AlwaysReturn();
-    loop();
-    Verify(Method(ArduinoFake(), delay).Using(100)).Exactly(2_Times);
+    //When(Method(ArduinoFake(), beep_motor)).AlwaysReturn();
+    setup();
+    Verify(Method(ArduinoFake(), pinMode).Using(5, INPUT));
+    Verify(Method(ArduinoFake(), digitalWrite).Using(5, HIGH));
+    Verify(Method(ArduinoFake(), analogReference).Using(EXTERNAL));
+    Verify(OverloadedMethod(ArduinoFake(Serial), begin, void(unsigned long)).Using(115200)).Once();
 }
 
 int main(int argc, char **argv)
@@ -21,6 +36,7 @@ int main(int argc, char **argv)
     UNITY_BEGIN();
 
     RUN_TEST(test_loop);
+    RUN_TEST(test_setup);
 
     UNITY_END();
 
